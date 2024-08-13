30bd4d0d831e65c6825829c5f62865f4f86894df
Ian Gabaraev <iangabaraev@Ians-MacBook-Pro.local>
2024-08-14 06:40:30 +0700

    push

--- docker/local-single/telegram/bot.py
+++ docker/local-single/telegram/bot.py
@@ -1,13 +1,5 @@
 import os
 import time
-
-from utils import (
-    get_cpu_temperature,
-    get_battery,
-    get_cpu_usage,
-    get_ram_usage,
-    get_system_uptime,
-)
 import telebot
 from dotenv import load_dotenv
 
@@ -19,25 +11,8 @@ main_chat_id = os.getenv("MAIN_CHAT_ID")
 bot = telebot.TeleBot(bot_token)
 
 
-# This needs to removed
-# Push stats metrics to cloudwatch
-# retrieve from bot in monitor service
-
-
 def send_on_startup():
     bot.send_message(main_chat_id, "Bot is running 🚀")
-
-
-@bot.message_handler(func=lambda message: message.text == "stats")
-def handle_message(message):
-    stats = f"""
-🌡️ CPU Temperature: {get_cpu_temperature()}°C
-🔋 Battery: {get_battery()}%
-📊 CPU Usage: {get_cpu_usage()}%
-💾 RAM Usage: {get_ram_usage()}
-⏳ System Uptime: {get_system_uptime()}
-"""
-    bot.reply_to(message, stats)
 
 
 if __name__ == "__main__":
